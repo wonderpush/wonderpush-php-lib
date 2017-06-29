00b09a0ed2a20c8e35fe35ca77306e72bf02d124
Olivier Favre <olivier@wonderpush.com>
2017-06-29 10:08:41 +0200

    PSR4 compliance

--- init.php
+++ init.php
@@ -1,29 +1,27 @@
 <?php
 
-// Util
-require(dirname(__FILE__) . '/lib/util/JsonSerializable.php');
-require(dirname(__FILE__) . '/lib/util/DefaultLogger.php');
-require(dirname(__FILE__) . '/lib/util/ArrayUtil.php');
-require(dirname(__FILE__) . '/lib/util/StringUtil.php');
-require(dirname(__FILE__) . '/lib/util/TimeUnit.php');
-require(dirname(__FILE__) . '/lib/util/TimeUtil.php');
-require(dirname(__FILE__) . '/lib/util/TimeValue.php');
-require(dirname(__FILE__) . '/lib/util/UrlUtil.php');
+// WonderPush\Util
+require(dirname(__FILE__) . '/lib/Util/JsonSerializable.php');
+require(dirname(__FILE__) . '/lib/Util/DefaultLogger.php');
+require(dirname(__FILE__) . '/lib/Util/ArrayUtil.php');
+require(dirname(__FILE__) . '/lib/Util/StringUtil.php');
+require(dirname(__FILE__) . '/lib/Util/TimeUnit.php');
+require(dirname(__FILE__) . '/lib/Util/TimeUtil.php');
+require(dirname(__FILE__) . '/lib/Util/TimeValue.php');
+require(dirname(__FILE__) . '/lib/Util/UrlUtil.php');
 
-// DTO
-require(dirname(__FILE__) . '/lib/dto/Object.php');
-require(dirname(__FILE__) . '/lib/dto/GeoLocation.php');
-require(dirname(__FILE__) . '/lib/dto/User.php');
-require(dirname(__FILE__) . '/lib/dto/Installation.php');
-require(dirname(__FILE__) . '/lib/dto/Event.php');
-require(dirname(__FILE__) . '/lib/dto/Notification.php');
-
-// Net
-require(dirname(__FILE__) . '/lib/net/Request.php');
-require(dirname(__FILE__) . '/lib/net/Response.php');
-require(dirname(__FILE__) . '/lib/net/HttpClientInterface.php');
-require(dirname(__FILE__) . '/lib/net/CurlHttpClient.php');
-
-// WonderPush class
-require(dirname(__FILE__) . '/lib/WonderPush.php');
+// WonderPush
 require(dirname(__FILE__) . '/lib/Rest.php');
+require(dirname(__FILE__) . '/lib/WonderPush.php');
+require(dirname(__FILE__) . '/lib/Object.php');
+require(dirname(__FILE__) . '/lib/GeoLocation.php');
+require(dirname(__FILE__) . '/lib/User.php');
+require(dirname(__FILE__) . '/lib/Installation.php');
+require(dirname(__FILE__) . '/lib/Event.php');
+require(dirname(__FILE__) . '/lib/Notification.php');
+
+// WonderPush\Net
+require(dirname(__FILE__) . '/lib/Net/Request.php');
+require(dirname(__FILE__) . '/lib/Net/Response.php');
+require(dirname(__FILE__) . '/lib/Net/HttpClientInterface.php');
+require(dirname(__FILE__) . '/lib/Net/CurlHttpClient.php');
