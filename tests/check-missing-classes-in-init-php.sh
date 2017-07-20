1191861d2c3baba8e55dfac65b2ac5ccacdfc402
Olivier Favre <olivier@wonderpush.com>
2017-07-20 12:46:59 +0200

    Test for missing PHP files in init.php

---
+++ tests/check-missing-classes-in-init-php.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+missingFiles="$(find lib/ -type f -name '*.php' | while read f; do grep -q -F "$f" init.php || echo "$f"; done)"
+
+if [ -n "$missingFiles" ]; then
+	echo "There are missing php files in init.php:"
+	echo "$missingFiles"
+	exit 1
+fi
