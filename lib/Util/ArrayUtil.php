51f16944866679ca6d43b8c86c74b5d043fb4353
Olivier Favre <olivier@wonderpush.com>
2017-07-25 13:19:07 +0200

    ArrayUtil tests

--- lib/Util/ArrayUtil.php
+++ lib/Util/ArrayUtil.php
@@ -7,25 +7,31 @@ namespace WonderPush\Util;
  */
 class ArrayUtil {
 
-  static public function flatten($a) {
-    if(!is_array($a)) return array();
+  public static function flatten($a) {
     $result = array();
-    foreach($a as $key => $v){
-      if(is_array($v)) $result = array_merge($result, self::flatten($v));
-      else $result[$key] = $v;
+
+    if (is_array($a)) {
+      foreach($a as $v) {
+        if (is_array($v)) {
+          $result = array_merge($result, self::flatten($v));
+        } else {
+          $result[] = $v;
+        }
+      }
     }
+
     return $result;
   }
 
-  static public function getIfSet($a, $i, $notSet = NULL) {
+  public static function getIfSet($a, $i, $notSet = NULL) {
     return isset($a[$i]) ? $a[$i] : $notSet;
   }
 
-  static public function is_not_null($var) {
+  public static function is_not_null($var) {
     return !is_null($var);
   }
 
-  static public function filterNulls($array) {
+  public static function filterNulls($array) {
     return array_filter($array, '\WonderPush\Util\ArrayUtil::is_not_null');
   }
 
--- tests/Util/ArrayUtilTest.php
+++ tests/Util/ArrayUtilTest.php
@@ -4,20 +4,97 @@ namespace WonderPush\Util;
 
 class ArrayUtilTest extends \WonderPush\TestCase {
 
-  public function testFlatten() {
-    // TODO test flatten
+  public function testFlattenNonArrayValues() {
+    $nonArrayReturnValue = array();
+    $this->assertEquals($nonArrayReturnValue, ArrayUtil::flatten(null));
+    $this->assertEquals($nonArrayReturnValue, ArrayUtil::flatten(true));
+    $this->assertEquals($nonArrayReturnValue, ArrayUtil::flatten(false));
+    $this->assertEquals($nonArrayReturnValue, ArrayUtil::flatten(0));
+    $this->assertEquals($nonArrayReturnValue, ArrayUtil::flatten(3.14));
+    $this->assertEquals($nonArrayReturnValue, ArrayUtil::flatten(''));
+    $this->assertEquals($nonArrayReturnValue, ArrayUtil::flatten('foo'));
+    $this->assertEquals($nonArrayReturnValue, ArrayUtil::flatten(new \stdClass()));
   }
 
-  public function testGetIfSet() {
-    // TODO test getIfSet
+  public function testFlatten() {
+    $this->assertEquals(array(       ), ArrayUtil::flatten(array(       )));
+    $this->assertEquals(array(0      ), ArrayUtil::flatten(array(0      )));
+    $this->assertEquals(array(3, 2, 1), ArrayUtil::flatten(array(3, 2, 1)));
+
+    $this->assertEquals(array(       ), ArrayUtil::flatten(array(array(       ))));
+    $this->assertEquals(array(0      ), ArrayUtil::flatten(array(array(0      ))));
+    $this->assertEquals(array(3, 2, 1), ArrayUtil::flatten(array(array(3, 2, 1))));
+
+    $this->assertEquals(array(       ), ArrayUtil::flatten(array(array(), array(       ), array())));
+    $this->assertEquals(array(0      ), ArrayUtil::flatten(array(array(), array(0      ), array())));
+    $this->assertEquals(array(3, 2, 1), ArrayUtil::flatten(array(array(), array(3, 2, 1), array())));
+
+    // Test that flatten elements do not overwrite elements at occupied positions
+    $this->assertEquals(array('00', '01', '1'), ArrayUtil::flatten(array(array('00', '01'), '1')));
+    $this->assertEquals(array('0', '10', '11'), ArrayUtil::flatten(array('0', array('10', '11'))));
+  }
+
+  public function testFlattenAssoc() {
+    $this->assertEquals(array('FOO'), ArrayUtil::flatten(array('foo' => 'FOO')));
+    $this->assertEquals(array('FOO', 'BAR.FOO'), ArrayUtil::flatten(array('foo' => 'FOO', 'bar' => array('foo' => 'BAR.FOO'))));
+  }
+
+  public function testGetIfSetScalars() {
+    $def = new \stdClass();
+    foreach (array(null, 42, true) as $value) { // no string to avoid "String offset cast occurred"
+      foreach (array(null, 42, true, 'foo', array(1,2,3)) as $key) {
+        $this->assertNull(ArrayUtil::getIfSet($value, $key));
+        $this->assertSame($def, ArrayUtil::getIfSet($value, $key, $def));
+      }
+    }
+  }
+
+  public function testGetIfSetArrays() {
+    $def = new \stdClass();
+
+    // null is converted to ''
+    $this->assertSame($def, ArrayUtil::getIfSet(array(), null, $def));
+    $this->assertSame($def, ArrayUtil::getIfSet(array(0), null, $def));
+    $this->assertEquals('', ArrayUtil::getIfSet(array('' => ''), null, $def));
+    $this->assertEquals('', ArrayUtil::getIfSet(array('' => ''), '', $def));
+
+    $this->assertEquals('FOO', ArrayUtil::getIfSet(array('foo' => 'FOO'), 'foo', $def));
+    $this->assertSame($def, ArrayUtil::getIfSet(array('foo' => 'FOO'), 'bar', $def));
+
+    $this->assertSame($def, ArrayUtil::getIfSet(array('foo', 'bar'), -1, $def));
+    $this->assertEquals('foo', ArrayUtil::getIfSet(array('foo', 'bar'), 0, $def));
+    $this->assertEquals('bar', ArrayUtil::getIfSet(array('foo', 'bar'), 1, $def));
+    $this->assertSame($def, ArrayUtil::getIfSet(array('foo', 'bar'), 2, $def));
+  }
+
+  public function testGetIfSetWithNullValues() {
+    $def = new \stdClass();
+
+    $this->assertSame($def, ArrayUtil::getIfSet(array('foo', null, 'bar'), 1, $def));
+    $this->assertSame($def, ArrayUtil::getIfSet(array('foo' => 'FOO', 'null' => null, 'bar' => 'BAR'), 'null', $def));
   }
 
   public function testIsNotNull() {
-    // TODO test is_not_null
+    $this->assertFalse(ArrayUtil::is_not_null(null));
+    $this->assertTrue(ArrayUtil::is_not_null('foo'));
+    $this->assertTrue(ArrayUtil::is_not_null(0));
+    $this->assertTrue(ArrayUtil::is_not_null(1));
+    $this->assertTrue(ArrayUtil::is_not_null(true));
+    $this->assertTrue(ArrayUtil::is_not_null(false));
+    $this->assertTrue(ArrayUtil::is_not_null(array()));
+    $this->assertTrue(ArrayUtil::is_not_null(new \stdClass()));
   }
 
   public function testFilterNulls() {
-    // TODO test filterNulls
+    // It does not renumber non-associative arrays
+    $this->assertEquals(array(0 => false, 2 => 1), ArrayUtil::filterNulls(array(false, null, 1)));
+
+    $this->assertEquals(array('foo' => 'FOO', 0), ArrayUtil::filterNulls(array('foo' => 'FOO', 'bar' => null, 0, null)));
+
+    // It does not work recursively
+    $this->assertEquals(array('foo' => array(null)), ArrayUtil::filterNulls(array('foo' => array(null))));
+    $this->assertEquals(array('foo' => array(0, null, 2)), ArrayUtil::filterNulls(array('foo' => array(0, null, 2))));
+    $this->assertEquals(array('foo' => array('foo' => null)), ArrayUtil::filterNulls(array('foo' => array('foo' => null))));
   }
 
 }
