c121841d6d908ba4a78707921d68422ae476484c
Olivier Favre <olivier@wonderpush.com>
2017-07-26 09:54:28 +0200

    Do not leave disabled autoloader after running tests
    It prevents from manually running scripts or phpunit.

--- build.php
+++ build.php
@@ -25,6 +25,10 @@ if ($returnStatus !== 0) {
 
 $config = $autoload ? 'phpunit.xml' : 'phpunit.no_autoload.xml';
 passthru("./vendor/bin/phpunit -c $config", $returnStatus);
+if (!$autoload) {
+  // Revert autoloader modifications made earlier
+  passthru('composer dump-autoload', $returnStatus);
+}
 if ($returnStatus !== 0) {
   exit(1);
 }
