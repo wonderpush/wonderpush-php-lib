e3115b699351396a5fb2f22da67a9e44a9aa962d
Stéphane JAIS <stephane@wonderpush.com>
2019-06-11 10:46:49 +0200

    fix typo

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
 // WonderPush\Util
 require(dirname(__FILE__) . '/lib/Util/JsonSerializable.php');
 require(dirname(__FILE__) . '/lib/Util/Logger.php');
-grequire(dirname(__FILE__) . '/lib/Util/DefaultLogger.php');
+require(dirname(__FILE__) . '/lib/Util/DefaultLogger.php');
 require(dirname(__FILE__) . '/lib/Util/ArrayUtil.php');
 require(dirname(__FILE__) . '/lib/Util/StringUtil.php');
 require(dirname(__FILE__) . '/lib/Util/TimeUnit.php');
